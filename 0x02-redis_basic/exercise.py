bb2932ec15319ed55112438f113d260f5f93288d
Daniel German <danielchuksgerman@gmail.com>
2024-05-15 11:41:57 +0000

    Recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module for Cache class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -16,3 +16,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int,
+                                                          float]:
+        """Getter method"""
+        if not self._redis.exists(key):
+            return None
+
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Get value in string format"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Get value in integer format"""
+        return self.get(key, fn=int)
